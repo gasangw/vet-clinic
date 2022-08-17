8f97fbd9832b1e595e6f8bbb42027c66b1e6b136
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-08-18 00:44:46 +0200

    update according to the questions

--- queries.sql
+++ queries.sql
@@ -9,3 +9,16 @@ SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 -- Second project
  ALTER TABLE animals ADD COLUMN species text;
+ UPDATE animals SET species = unspecified;
+ROLLBACK;
+UPDATE animals SET species ='digimon' WHERE name LIKE '%mon%';
+UPDATE animals SET species = 'pokemon' WHERE species <> 'digimon';
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+DELETE FROM animals WHERE date_of_birth::date > '2022-01-01';
+SAVEPOINT FRSTDELETE;
+UPDATE animals SET weight_kg = weight_kg*-1;
