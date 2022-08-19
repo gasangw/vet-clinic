f0ab9c8ab369a74ad45d49d7902088d50b7c1e1b
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-08-19 23:41:46 +0200

    add tables and insert data

--- schema.sql
+++ schema.sql
@@ -9,3 +9,27 @@ CREATE TABLE animals (
     weight_kg decimal,
     species text
 );
+
+-- creating tables
+
+CREATE TABLE owners (
+    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+    full_name text, 
+    age integer
+);
+
+CREATE TABLE species (
+    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+    name VARCHAR(200)
+);
+
+-- Modify animals table:
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id integer references species(id);
+ALTER TABLE animals ADD COLUMN owner_id integer references owners(id);
+
+-- Insert the following data into the species table
+INSERT INTO species (name) VALUES('Pokemon');
+INSERT INTO species (name) VALUES('Digimon');
+
+-- Modify your inserted animals so it includes the species_id value
